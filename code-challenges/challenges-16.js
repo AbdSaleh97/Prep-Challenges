78d05d91c24f4a713feb715596d3099b6bb5c076
AbdSaleh97 <salehabd.897@gmail.com>
2024-02-18 02:49:07 +0300

    challenge 16

---
+++ code-challenges/challenges-16.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+// Combine methods
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Write a function that takes in a string and returns the reverse of this string .
+//
+// Input <= "Hello";
+// Output => "olleH";
+
+const reverseString = (string) => {
+    return string.split('').reverse().join('');
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes an array of strings and returns only strings that contain ^_^.
+//
+// Ex :-
+// Input <= ["hello ^_^ ","Hi ^_^" ,"What's up ^_-" ,"lol"] , Output => ["hello ^_^ ","Hi ^_^" ] ;
+
+const detectFace = (arr) => {
+    return arr.filter(el=> el.includes('^_^'))
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes in a string and returns a string contains only characters in even positions.
+//
+// Ex :-
+// Input <= "coding" output =>"cdn"
+
+const eveCharacter = (str) => {
+    return Array.from(str).filter((el, i) => i % 2 === 0).join('');
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04
+// Optional:
+
+// Let us consider you are hired as a software developer in a tech company and you are assigned to work on a restaurant web application project .
+
+// And you are assigned to write function to return only ingredients that contains chicken.
+//
+// Input:
+// [["mushroom", "grilled chicken", "sauce"], ["Bread", "Potato", "baked chicken"], ["fried potato", "garlic sauce", "fried chicken"]];
+//
+// Output:
+// [["grilled chicken"], ["baked chicken"], ["fried chicken"]];
+
+
+const chickenGradients = (arr) => {
+    return arr.map(el => el.filter(val => val.includes("chicken")));
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { reverseString, detectFace, eveCharacter, chickenGradients };
